8b1740e7c44dd6d2586b5f698c297889d53d1568
vidhi Rajvanshi <101122567+programvidhi89@users.noreply.github.com>
2022-07-29 22:18:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+var buttonColors = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+$(".dice").click(function(){
+   
+    var userChoosenColor = $(this).attr("id");
+    userClickedPattern.push(userChoosenColor);
+    playSound(userChoosenColor);
+    animatePress(userChoosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+    console.log('user',userChoosenColor, userClickedPattern);
+
+});
+
+function checkAnswer(currentLevel){
+    console.log("current",gamePattern, gamePattern[currentLevel], userClickedPattern[currentLevel])
+    if(gamePattern[currentLevel]==userClickedPattern[currentLevel]){
+        if(userClickedPattern.length == gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        playSound("wrong");
+        $(".container").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function(){
+            $(".container").removeClass("game-over");
+        },200);
+    }
+    startOver();
+}
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber  = Math.floor(Math.random()*4);
+    var randomChoosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChoosenColor);
+    $("#"+randomChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChoosenColor);
+    console.log("game", gamePattern);
+}
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+      }, 100);
+    
+}
+function playSound(currentColor){
+    var audio = new Audio("sounds/"+currentColor+".mp3");
+    audio.play();
+}
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
